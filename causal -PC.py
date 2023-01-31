a0d2ef51b43faa101b9ae4446bfcc658a4117abf
unknown <2321759131@qq.com>
2023-01-31 12:36:14 +0800

    aa

--- causal -PC.py
+++ causal -PC.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 data=pd.read_csv("chosen_data.csv")
-datat=data[['CLDN11','HGF','CDH1','SEZ6','MLH1','DOCK3','SEZ6L','VTN','EPM2A','CD274','SMAD9','ACVR2A','F12','VEGFC','ALK','TCEA2','CTSF','SEZ6L2','SULT1C4','MSS','MSI-H','MSI-L','ZNF43']]
+datat=data[['CLDN11','HGF','CDH1','SEZ6','MLH1','DOCK3','SEZ6L','VTN','EPM2A','CD274','SMAD9','ACVR2A','F12','VEGFC','ALK','TCEA2','CTSF','SEZ6L2','SULT1C4','MSS-ALL','ZNF43']]
 glasso = cdt.independence.graph.Glasso()
 skeleton = glasso.predict(datat)
 new_skeleton = cdt.utils.graph.remove_indirect_links(skeleton, alg='aracne')
