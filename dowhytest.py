fd76fdecb58ebc5b93b244b5c7f2eb26c01fe800
unknown <2321759131@qq.com>
2023-02-02 00:22:57 +0800

    remove ui

--- dowhytest.py
+++ dowhytest.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import dowhy
 
 data=pd.read_csv("chosen_data.csv")
-datat=data[['CLDN11','HGF','CDH1','SEZ6','MLH1','DOCK3','SEZ6L','VTN','EPM2A','CD274','SMAD9','ACVR2A','F12','VEGFC','ALK','TCEA2','CTSF','SEZ6L2','SULT1C4','MSS-Normal','ZNF43']]
+datat=data[['CLDN11','HGF','CDH1','SEZ6','MLH1','DOCK3','SEZ6L','VTN','EPM2A','CD274','SMAD9','ACVR2A','F12','VEGFC','ALK','TCEA2','CTSF','SEZ6L2','SULT1C4','MSS-ALL','ZNF43']]
 glasso = cdt.independence.graph.Glasso()
 skeleton = glasso.predict(datat)
 new_skeleton = cdt.utils.graph.remove_indirect_links(skeleton, alg='aracne')
@@ -28,7 +28,7 @@ nx.draw_networkx(output_graph, font_size=5)
 plt.show()
 #print(nx.adjacency_matrix(output_graph).todense())
 
-model_dowhy=dowhy.causal_model.CausalModel(datat,treatment="F12",outcome="MSS-Normal",graph=gmlstring)
+model_dowhy=dowhy.causal_model.CausalModel(datat,treatment="MLH1",outcome="MSS-ALL",graph=gmlstring)
 model_dowhy.view_model()
 identified_estimand = model_dowhy.identify_effect()
 print(identified_estimand)
