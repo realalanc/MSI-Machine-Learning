461d311cf58485264bf2fd5770723befdc5d4d84
realalanc <1245846284@qq.com>
2023-02-05 17:32:45 +0800

    trial of debug

--- MSItool.py
+++ MSItool.py
@@ -44,19 +44,25 @@ correlation=0
 algorithm=0
 evalution=0
 filename=""
+writedir="" #貌似也是作用域的问题，不在这里写上的话后面就会认为writedir只在那个for里面定义
 data=0
-msiname="MSI"
+msiname="MSI-ALL"
 args=[]
 opts=[]
 try:
-    opts,args=getopt.gnu_getopt(sys.argv[1:],"-m:-o:-v-h-c:-e:-a",["name=","output=","version","help","corr=","alg=","evl="])
+    opts,args=getopt.gnu_getopt(sys.argv[1:],"-m:-o:-v-h-c:-a:-e:",["name=","output=","version","help","corr=","alg=","evl="])#这里最后那个-e没有加冒号，a和e还写反了
 except getopt.GetoptError as err:
     print(str(err))
     exit_help()
 def readdata():
     try:
-        filename=args[0]
-        data=pd.read_csv("chosen_data.csv")
+        #filename=args[0] 这里应该是sys.argv[0]？args的0不是filename吧
+        global filename #还有就是函数内用全局变量要声明global，下面的data同理
+        filename=sys.argv[0]
+        global data
+        data=pd.read_csv("new_chosen_data.csv") #这里硬编码死输入的csv名还是搞成输入参数？我这里改了是因为，原来的chosen_data有好多非数项不能跑
+        #最重要的一点:数据预处理必须扔到非数项！包括sample的代号，除了index都要是数才行
+        data=data.dropna()
     except:
         print("you need to choose a csv file as input")
         exit()
@@ -86,7 +92,7 @@ for o,a in opts:
 readdata()
 if msiname in data.index:
     data=data.T
-if msiname not in data.column:
+if msiname not in data.columns: #这里原来写成了column
     print("invalid msi column(or index) name")
     exit()
 
@@ -98,15 +104,16 @@ except:
     pass
 
 def getcorrelation():
+    n=100 #后面的那个n参数没有定义，这里应该是指“取前多少个”吧，我在这里先写成常量了，或者改成用户选择？
     nrows=data.columns.get_loc(msiname)+1
     if correlation==1:
         dcorr=data.corr()
-        dcorr.to_csv(writedir+'/corr_result.csv')
+        dcorr.to_csv('corr_result.csv')#路径的问题和下面一样
         # dcorr=pd.read_csv("corr_result.csv",header=0,index_col=0)
         a=np.triu(dcorr,1)
         a=a.flatten()
         topn=heapq.nlargest(n, range(len(a)), a.take)
-        with open(writedir+'/corrdata.txt', 'w') as f:
+        with open('corrdata.txt', 'w') as f:
             for i in topn:
                 f.write((dcorr.index)[(i+1)//(nrows-1)]);f.write("    ")
                 f.write((dcorr.index)[(i+1)%(nrows-1)-1]);f.write("    ")
@@ -122,19 +129,19 @@ def getcorrelation():
                 o_index.append(i)
                 o_pv.append(p)
         corr.sort(reverse=True)
-        with open(writedir+'/corrdata.txt', 'w') as f:
+        with open('corrdata.txt', 'w') as f:
             for i in range(0,len(corr)):
                 f.write(data.columns[o_index[o_corr.index(corr[i])]]);f.write("    p=")
                 f.write(f"{o_pv[o_corr.index(corr[i])]}");f.write("    ")
                 f.write(f"{corr[i]}\n")
-    with open(writedir+'/kendall_result.txt', 'w') as f:
+    with open('kendall_result.txt', 'w') as f:
             f.write('kendalltau  r and p\n\n')
             r, p = scipy.stats.kendalltau(data.iloc[:, nrows - 1], data.iloc[:, nrows])
             f.write('Primary Site and MSI\n');f.write(f"{r}");f.write("    ");f.write(f"{p}\n\n")
 def getgraph():
     import cdt
     glasso = cdt.independence.graph.Glasso()
-    skeleton = glasso.predict()
+    skeleton = glasso.predict(data)
     new_skeleton = cdt.utils.graph.remove_indirect_links(skeleton, alg='aracne')
     if(algorithm==1):
         model = cdt.causality.graph.CCDr()
@@ -144,14 +151,14 @@ def getgraph():
         model=cdt.causality.graph.GES()
     output_graph=model.predict(data)
     nx.draw_networkx(output_graph, font_size=5)
-    plt.savefig(writedir+"/graph.png",format="PNG")
+    plt.savefig("graph.png",format="PNG")
     plt.show()
     gmlgenerator=nx.generate_gml(output_graph)
     gmllist=[]
     for i in gmlgenerator:
         gmllist.append(i)
     gmlstring="".join(gmllist)
-    with open(writedir+"/graph.txt","w") as f:
+    with open("graph.txt","w") as f:#这里如果用加上writedir的绝对路径的话会报no such file，反正就是在一个工作目录下，没必要
         f.write(gmlstring)
     return output_graph
 
@@ -170,10 +177,12 @@ def dowhytest(graph):
     tt=input()
     oc=input()
     try:
-        model_dowhy=dowhy.causal_model.CausalModel(data,treatment="tt",outcome="oc",graph=gmlstring)
+        model_dowhy=dowhy.causal_model.CausalModel(data,treatment=tt,outcome=oc,graph=gmlstring) #这里的tt和oc不应该打引号
         identified_estimand = model_dowhy.identify_effect()
-        with open(writedir+"/dowhy_identified_estimand.txt","w") as f:
-            f.write(identified_estimand)
+        with open("dowhy_identified_estimand.txt","w",encoding='utf-8') as f:
+            print(str(identified_estimand))
+
+            f.write(str(identified_estimand))  #write()只能写一个str，没有重载写identified_estimand的,并且由于编码的问题需要指定为utf-8
         print("result of identify effect has been written into identified_estimand.txt")
     except:
         print("invalid input edge!")
@@ -190,18 +199,18 @@ def dowhytest(graph):
         refute3_results=model_dowhy.refute_estimate(identified_estimand, estimate,
             method_name="data_subset_refuter")
         #print(refute3_results)
-        with open(writedir+"/evalution.txt","w") as f:
+        with open("evalution.txt","w",encoding='utf-8') as f:#还是那个问题：如果是全局路径就莫名会no such file
             f.write("estimate:\n")
-            f.write(estimate)
+            f.write(str(estimate))
             f.write("\n----------------------------\n----------------------------\n")
             f.write("random common cause method result:\n")
-            f.write(refute1_results)
+            f.write(str(refute1_results))
             f.write("\n----------------------------\n----------------------------\n")
             f.write("placebo treatment refuter method result\n")
-            f.write(refute2_results)
+            f.write(str(refute2_results))
             f.write("\n----------------------------\n----------------------------\n")
             f.write("data subset refuter method result\n")
-            f.write(refute3_results)
+            f.write(str(refute3_results))
     elif evalution==2:
         estimate_iv=model_dowhy.estimate_effect(identified_estimand, method_name="iv.instrumental_variable")
         #print(estimate)
@@ -211,16 +220,16 @@ def dowhytest(graph):
         refute2_results=model_dowhy.refute_estimate(identified_estimand, estimate_iv,
             method_name="data_subset_refuter")
         #print(refute3_results)
-        with open(writedir+"/evalution.txt","w") as f:
+        with open("evalution.txt","w",encoding='utf-8') as f:
             f.write("estimate:\n")
-            f.write(estimate_iv)
+            f.write(str(estimate_iv))
             f.write("\n----------------------------\n----------------------------\n")
             f.write("random common cause method result:\n")
-            f.write(refute1_results)
+            f.write(str(refute1_results))
             f.write("\n----------------------------\n----------------------------\n")
             f.write("data subset refuter method result\n")
-            f.write(refute2_results)
-
+            f.write(str(refute2_results))
+    print("result of evalution has been written into evalution.txt")
 if correlation!=0:
     getcorrelation()
     exit()
