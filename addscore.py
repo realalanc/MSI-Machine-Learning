dd8a9eabe27f92cea3a800bf16a3bf1dca2da31d
realalanc <1245846284@qq.com>
2023-03-17 00:25:58 +0800

    scored

--- addscore.py
+++ addscore.py
@@ -1,2 +1,11 @@
 import pandas as pd
-score=pd.read_csv("")
+score=pd.read_csv("sup_table3_caseinfo_csved.csv",header=0,index_col=0)
+print(score.head())
+olddata=pd.read_csv("DESeq2.csv",header=0,index_col=0)
+scorelist=[]
+for i in olddata:
+    for j in score.index:
+        if(i==j):
+            scorelist.append(score.loc[j,"MSIsensor-pro(all)"])
+scorelistdf=pd.DataFrame(scorelist).T
+scorelistdf.to_csv("MSIScore.csv")
